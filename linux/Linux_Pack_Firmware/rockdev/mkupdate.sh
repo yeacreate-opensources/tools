12a988e0107d1770e898ca0546976964224a3bfb
simon <simon@yeacreate.com>
2018-12-30 11:15:29 +0800

    add the ntablet branch

--- linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
+++ linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
@@ -15,7 +15,7 @@ if [ ! -f "package-file" ]; then
 	exit 1
 fi
 ./afptool -pack ./ Image/update.img || pause
-./rkImageMaker -RK312A Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
+./rkImageMaker -RK320A Image/rk3288_loader_v1.06.236.bin Image/update.img update.img -os_type:androidos || pause
 echo "Making update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
